9299d23763d831406b47eda90375979315da26e9
stilz <121803937+stiles667@users.noreply.github.com>
2026-08-25 10:53:03 +0200

    Suppression des fichiers temporaires make et .gitkeep

--- make.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-REM make.bat - Wrapper pour ex\u00e9cuter les commandes du Makefile sous Windows
-REM Usage: make help, make tf.init
-
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0make.ps1" %*
-exit /b %ERRORLEVEL%
